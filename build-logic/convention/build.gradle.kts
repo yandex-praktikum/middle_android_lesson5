95cf17ae127c7927c15d31e880a350f4536c3508
Дмитрий Гордеев <netimen2@yandex.ru>
2025-02-03 09:45:51 +0300

    working build

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -4,42 +4,27 @@ plugins {
     `kotlin-dsl`
 }
 
-group = "yandex.practicum"
-
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
+        jvmTarget = JvmTarget.JVM_11
     }
 }
 
 dependencies {
     compileOnly(libs.android.gradlePlugin)
-    compileOnly(libs.android.tools.common)
     compileOnly(libs.kotlin.gradlePlugin)
-
-//    compileOnly(libs.ksp.gradlePlugin)
-//    compileOnly(libs.android.gradlePlugin)
-//    compileOnly(libs.android.tools.common)
-//    compileOnly(libs.compose.gradlePlugin)
-//    compileOnly(libs.firebase.crashlytics.gradlePlugin)
-//    compileOnly(libs.firebase.performance.gradlePlugin)
-//    compileOnly(libs.kotlin.gradlePlugin)
-//    compileOnly(libs.ksp.gradlePlugin)
-//    compileOnly(libs.room.gradlePlugin)
-//    implementation(libs.truth)
-//    lintChecks(libs.androidx.lint.gradle)
 }
 
 
 gradlePlugin {
     plugins {
         register("androidFeature") {
-            id = libs.plugins.yandex.practicum.android.feature.get().pluginId
+            id = libs.plugins.conventionsample.android.feature.get().pluginId
             implementationClass = "AndroidFeatureConventionPlugin"
         }
     }
--- build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -1,28 +1,20 @@
-package yandex.practicum.conventionsample.build_logic.convention
-
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
-
-fun DependencyHandler.library(name: String) =
-    extensions.getByType<VersionCatalogsExtension>().named("libs").findLibrary(name)
-//fun DependencyHandler.implementation(dependencyNotation: Any): Dependency? =
-//    add("implementation", dependencyNotation)
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("com.android.library")
             pluginManager.apply("org.jetbrains.kotlin.android")
+            pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
 
             extensions.configure<LibraryExtension> {
                 compileSdk = 35
@@ -32,31 +24,30 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 }
                 compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_17
-                    targetCompatibility = JavaVersion.VERSION_17
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
                 }
                 buildFeatures {
                     compose = true
                 }
-
-//                composeOptions {
-//                    kotlinCompilerExtensionVersion =
-//                        libs.findVersion("androidxComposeCompiler").get().toString()
-//                }
             }
             extensions.configure<KotlinAndroidProjectExtension> {
                 compilerOptions {
-                    jvmTarget = JvmTarget.JVM_17
+                    jvmTarget = JvmTarget.JVM_11
                 }
             }
 
             dependencies {
                 add("implementation", platform(library("androidx.compose.bom")))
-                add("implementation", library("androidx.compose.material3"))
-//                add("implementation", library("androidx-appcompat"))
+                add("implementation", library("androidx.material3"))
 
                 add("implementation", project(":core:analytics"))
             }
         }
     }
 }
+
+fun Project.library(alias: String) =
+    extensions.getByType(VersionCatalogsExtension::class.java).named("libs").findLibrary(alias)
+        .get()
+
